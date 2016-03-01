1d81528e2dbca0822754910c74da3c93d992aa52
muupu <once645@gmail.com>
2016-03-01 15:09:26 +0800

    add onOptionsItemSelected to menu

--- ActivityTest/app/src/main/java/com/test/activitytest/MainActivity.java
+++ ActivityTest/app/src/main/java/com/test/activitytest/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -43,6 +44,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "you clicked Add in menu", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "you clicked Remove in menu", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
         return true;
     }
 
