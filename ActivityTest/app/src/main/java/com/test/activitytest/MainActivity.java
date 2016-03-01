e6963bb78ea977d4f11dbbd0ee6f118da2225fa5
muupu <once645@gmail.com>
2016-03-01 16:09:15 +0800

    Intent to another application

--- ActivityTest/app/src/main/java/com/test/activitytest/MainActivity.java
+++ ActivityTest/app/src/main/java/com/test/activitytest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.test.activitytest;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -43,6 +44,23 @@ public class MainActivity extends AppCompatActivity {
 //                startActivity(intent);
 
                 Intent intent = new Intent("com.test.activitytest.Action_START1");
+                intent.addCategory("com.test.activitytest.MY_CATEGORY1");
+                startActivity(intent);
+            }
+        });
+
+        Button button11 = (Button)findViewById(R.id.button11);
+        button11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "you clicked button1",
+//                        Toast.LENGTH_SHORT).show();
+
+//                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+//                startActivity(intent);
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
                 startActivity(intent);
             }
         });
