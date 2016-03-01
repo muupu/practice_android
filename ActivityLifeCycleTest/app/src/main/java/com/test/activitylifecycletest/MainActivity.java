a8155902f208ae8f457c1d6782fd46bfde809042
muupu <once645@gmail.com>
2016-03-01 18:04:02 +0800

    add log to 7 life cycle

--- ActivityLifeCycleTest/app/src/main/java/com/test/activitylifecycletest/MainActivity.java
+++ ActivityLifeCycleTest/app/src/main/java/com/test/activitylifecycletest/MainActivity.java
@@ -34,4 +34,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
 }
