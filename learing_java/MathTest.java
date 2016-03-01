e1d340aa83b530c312b3b143cfac653a0f03df5f
muupu <once645@gmail.com>
2016-03-01 22:38:36 +0800

    add StringTest.java

--- learing_java/MathTest.java
+++ learing_java/MathTest.java
@@ -5,5 +5,9 @@ public class MathTest
 		double x = 4;
 		double y = Math.sqrt(x);
 		System.out.println(y);
+
+		int n = 123456789;
+		float f= n; // f is 1.23456792E8
+		System.out.println(f);
 	}
 }
