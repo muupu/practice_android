f00cf67116198b23688c0c3f8baaedfeb0b2f3b7
muupu <once645@gmail.com>
2016-03-01 17:58:31 +0800

    add normal and dialog activity

--- ActivityLifeCycleTest/app/src/main/java/com/test/activitylifecycletest/MainActivity.java
+++ ActivityLifeCycleTest/app/src/main/java/com/test/activitylifecycletest/MainActivity.java
@@ -1,13 +1,37 @@
 package com.test.activitylifecycletest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
+
+        Button startNormalActivity = (Button)findViewById(R.id.start_normal_button);
+        startNormalActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NormalActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button startDialogActivity = (Button)findViewById(R.id.start_dialog_button);
+        startDialogActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DialogActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
