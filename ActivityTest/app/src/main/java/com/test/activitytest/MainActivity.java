e54871d01386b50fde636e6ed68ac7387b02e7b7
muupu <once645@gmail.com>
2016-03-01 15:30:11 +0800

    use intent

--- ActivityTest/app/src/main/java/com/test/activitytest/MainActivity.java
+++ ActivityTest/app/src/main/java/com/test/activitytest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.test.activitytest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -35,8 +36,10 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "you clicked button1",
-                        Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "you clicked button1",
+//                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
             }
         });
     }
