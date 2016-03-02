5932d135f5a40eec51ac98e303de39b71b7a2371
muupu <once645@gmail.com>
2016-03-02 15:34:15 +0800

    ProgressBar test

--- UIWidgetTest/app/src/main/java/com/test/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/test/uiwidgettest/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -14,12 +15,14 @@ public class MainActivity extends Activity {
     private Button button;
     private EditText editText;
     private ImageView imageView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        progressBar = (ProgressBar)findViewById(R.id.progress_bar);
         imageView = (ImageView)findViewById(R.id.image_view);
         editText = (EditText)findViewById(R.id.edit_text);
         button = (Button)findViewById(R.id.button);
@@ -30,7 +33,17 @@ public class MainActivity extends Activity {
                     case R.id.button:
 //                        String inputText = editText.getText().toString();
 //                        Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
-                        imageView.setImageResource(R.drawable.wt_9348);
+
+//                        imageView.setImageResource(R.drawable.wt_9348);
+
+//                        if (progressBar.getVisibility() == View.GONE) {
+//                            progressBar.setVisibility(View.VISIBLE);
+//                        } else {
+//                            progressBar.setVisibility(View.GONE);
+//                        }
+                        int progress = progressBar.getProgress();
+                        progress += 10;
+                        progressBar.setProgress(progress);
                         break;
                     default:
                         break;
