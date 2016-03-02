8bc22904d6bcb14506c22f51b4a50c03917cc738
muupu <once645@gmail.com>
2016-03-02 15:06:36 +0800

    Button test

--- UIWidgetTest/app/src/main/java/com/test/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/test/uiwidgettest/MainActivity.java
@@ -3,12 +3,24 @@ package com.test.uiwidgettest;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
