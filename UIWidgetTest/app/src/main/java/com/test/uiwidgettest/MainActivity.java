9ad97a552f4cb04b61287b3a3f4c6125679210ce
muupu <once645@gmail.com>
2016-03-02 15:16:58 +0800

    Button and EditText test

--- UIWidgetTest/app/src/main/java/com/test/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/test/uiwidgettest/MainActivity.java
@@ -5,21 +5,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     private Button button;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editText = (EditText)findViewById(R.id.edit_text);
         button = (Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                switch (v.getId()) {
+                    case R.id.button:
+                        String inputText = editText.getText().toString();
+                        Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
             }
         });
     }
