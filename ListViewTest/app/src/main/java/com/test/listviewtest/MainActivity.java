c58b69cb7cd82a8518646f3eb4fe123775c9235c
muupu <once645@gmail.com>
2016-03-02 17:38:27 +0800

    ListView test

--- ListViewTest/app/src/main/java/com/test/listviewtest/MainActivity.java
+++ ListViewTest/app/src/main/java/com/test/listviewtest/MainActivity.java
@@ -2,12 +2,32 @@ package com.test.listviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] data = {
+            "Apple",
+            "Banana",
+            "Orange",
+            "Watermelon",
+            "Pear",
+            "Grape",
+            "Pineapple",
+            "Strawberry",
+            "Cherry",
+            "Mango"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                MainActivity.this, android.R.layout.simple_list_item_1, data);
+        ListView listView = (ListView)findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
     }
 }
