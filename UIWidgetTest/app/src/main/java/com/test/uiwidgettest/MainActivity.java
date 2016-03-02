8977cb9f8b5f17f510f8bc7ddfdf55c40edb2eb3
muupu <once645@gmail.com>
2016-03-02 15:11:30 +0800

    EditText test

--- UIWidgetTest/app/src/main/java/com/test/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/test/uiwidgettest/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends Activity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
     }
