6b345d124ad6e8fbd5d6d5c61bf6957c214a0abb
muupu <once645@gmail.com>
2016-03-03 15:41:02 +0800

    abortBroadcast test

--- BroadcastTest/app/src/main/java/com/test/broadcasttest/MyBroadcastReceiver.java
+++ BroadcastTest/app/src/main/java/com/test/broadcasttest/MyBroadcastReceiver.java
@@ -13,5 +13,6 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "received in MyBroadcastReceiver", Toast.LENGTH_SHORT).show();
+        abortBroadcast();
     }
 }
