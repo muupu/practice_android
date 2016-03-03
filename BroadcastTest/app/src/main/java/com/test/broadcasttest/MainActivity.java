5609fade724a4c5624c7fab6878a007a8802ad62
muupu <once645@gmail.com>
2016-03-03 15:29:58 +0800

    5.3.2 init BroadcastTest2 project

--- BroadcastTest/app/src/main/java/com/test/broadcasttest/MainActivity.java
+++ BroadcastTest/app/src/main/java/com/test/broadcasttest/MainActivity.java
@@ -9,6 +9,8 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -26,6 +28,15 @@ public class MainActivity extends Activity {
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         networkChangeReceiver = new NetworkChangeReceiver();
         registerReceiver(networkChangeReceiver, intentFilter);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.test.broadcasttest.MY_BROADCAST");
+                sendBroadcast(intent);
+            }
+        });
     }
 
     @Override
