7a5c296a37a10d8ee4bb27e644c03d9ddd54e876
muupu <once645@gmail.com>
2016-03-03 16:40:55 +0800

    update UI in subthread test

--- AndroidThreadTest/app/src/main/java/com/test/androidthreadtest/MainActivity.java
+++ AndroidThreadTest/app/src/main/java/com/test/androidthreadtest/MainActivity.java
@@ -3,12 +3,39 @@ package com.test.androidthreadtest;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private TextView text;
+
+    private Button changeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView)findViewById(R.id.text);
+        changeText = (Button)findViewById(R.id.change_text);
+        changeText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.change_text:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        text.setText("Nice to meet you"); // 在子线程中更新UI
+                    }
+                }).start();
+                break;
+            default:
+                break;
+        }
     }
 }
