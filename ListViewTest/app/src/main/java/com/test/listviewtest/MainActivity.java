04965e4cd15ebc2c917eda27ae92e80ff1221f60
muupu <once645@gmail.com>
2016-03-03 10:18:31 +0800

    ListView onItemClick test

--- ListViewTest/app/src/main/java/com/test/listviewtest/MainActivity.java
+++ ListViewTest/app/src/main/java/com/test/listviewtest/MainActivity.java
@@ -3,8 +3,11 @@ package com.test.listviewtest;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,6 +42,13 @@ public class MainActivity extends Activity {
 
         ListView listView = (ListView)findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fruit fruit = fruitList.get(position);
+                Toast.makeText(MainActivity.this, fruit.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void initFruits() {
