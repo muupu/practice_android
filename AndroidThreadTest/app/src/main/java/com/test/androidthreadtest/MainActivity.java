458cc89159d8d6577f701397f8d7d1492e69c4f4
muupu <once645@gmail.com>
2016-03-03 16:50:19 +0800

    Handler Message test

--- AndroidThreadTest/app/src/main/java/com/test/androidthreadtest/MainActivity.java
+++ AndroidThreadTest/app/src/main/java/com/test/androidthreadtest/MainActivity.java
@@ -1,17 +1,35 @@
 package com.test.androidthreadtest;
 
 import android.app.Activity;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 public class MainActivity extends Activity implements View.OnClickListener {
+
+    public static final int UPDATE_TEXT = 1;
 
     private TextView text;
 
     private Button changeText;
+
+    private Handler handler = new Handler() {
+
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case UPDATE_TEXT:
+                    text.setText("Nice to meet you");
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +48,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        text.setText("Nice to meet you"); // 在子线程中更新UI
+//                        text.setText("Nice to meet you"); // 在子线程中更新UI
+                        Message message = new Message();
+                        message.what = UPDATE_TEXT;
+                        handler.sendMessage(message);
                     }
                 }).start();
                 break;
