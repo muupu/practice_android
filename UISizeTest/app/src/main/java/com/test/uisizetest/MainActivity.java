6f516eeb2704ed2c24008d5b14bd20723afdafa1
muupu <once645@gmail.com>
2016-03-03 10:32:00 +0800

    dp sp test

--- UISizeTest/app/src/main/java/com/test/uisizetest/MainActivity.java
+++ UISizeTest/app/src/main/java/com/test/uisizetest/MainActivity.java
@@ -3,6 +3,7 @@ package com.test.uisizetest;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        float xdpi = getResources().getDisplayMetrics().xdpi;
+        float ydpi = getResources().getDisplayMetrics().ydpi;
+        Log.d("MainActivity", "xdpi is " + xdpi);
+        Log.d("MainActivity", "ydpi is " + ydpi);
     }
 }
