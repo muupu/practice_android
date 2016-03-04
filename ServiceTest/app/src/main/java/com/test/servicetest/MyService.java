34b3144e8633dcb48f643e0915979d274c655174
muupu <once645@gmail.com>
2016-03-04 09:55:26 +0800

    bind service test

--- ServiceTest/app/src/main/java/com/test/servicetest/MyService.java
+++ ServiceTest/app/src/main/java/com/test/servicetest/MyService.java
@@ -2,6 +2,7 @@ package com.test.servicetest;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -10,9 +11,23 @@ import android.util.Log;
  */
 public class MyService extends Service {
 
+    private DownloadBinder mBinder = new DownloadBinder();
+
+    class DownloadBinder extends Binder {
+
+        public void startDownload() {
+            Log.d("MyService", "startDownload executed");
+        }
+
+        public int getProgress() {
+            Log.d("MyService", "getProgress executed");
+            return 0;
+        }
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return mBinder;
     }
 
     @Override
