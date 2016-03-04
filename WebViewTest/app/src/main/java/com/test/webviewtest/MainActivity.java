f8cd72044d015e6bb1de3645d47e58cb20a2bb0c
muupu <once645@gmail.com>
2016-03-04 11:34:05 +0800

    WebView test

--- WebViewTest/app/src/main/java/com/test/webviewtest/MainActivity.java
+++ WebViewTest/app/src/main/java/com/test/webviewtest/MainActivity.java
@@ -3,12 +3,21 @@ package com.test.webviewtest;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.baidu.com");
     }
 }
