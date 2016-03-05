34c9e5cad305f1db994449cf2549f4fc9405e43f
muupu <once645@gmail.com>
2016-03-05 22:51:08 +0800

    add StackTraceTest

---
+++ CoreJava_Book/StackTraceTest.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class StackTraceTest
+{
+	public static int factorial(int n)
+	{
+		System.out.println("factorial(" + n + "):");
+		Throwable t = new Throwable();
+		StackTraceElement[] frames = t.getStackTrace();
+		for (StackTraceElement f : frames)
+		{
+			System.out.println(f);
+		}
+		int r;
+		if (n <= 1) r = 1;
+		else r = n * factorial(n-1);
+		System.out.println("return " + r);
+		return r;
+	}
+
+	public static void main(String[] args)
+	{
+		factorial(3);
+	}
+}
