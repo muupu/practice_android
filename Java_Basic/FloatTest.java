816977c6a649a3681125934fb7d53242bc453ac1
muupu <once645@gmail.com>
2016-04-05 14:21:34 +0800

    add FloatTest.java

---
+++ Java_Basic/FloatTest.java
@@ -0,0 +1,16 @@
+
+public class FloatTest {
+
+	public static void main(String[] args) {
+		double progress = 100f * 99 / 100;
+		String progressStr;
+		if (progress <= 0) {
+			progressStr = "0";
+		} else if (progress >= 100) {
+			progressStr = "100";
+		} else {
+			progressStr = String.format("%.1f", progress);
+		}
+		System.out.println("update..." + progress + "%");
+	}
+}
