81ae44ce162bc48a4bf6953ae9f4c0c7dfe109e1
muupu <once645@gmail.com>
2016-04-07 11:31:22 +0800

    Java泛型：类型变量的限定

--- CoreJava_Book/12generic/PairTest.java
+++ CoreJava_Book/12generic/PairTest.java
@@ -29,4 +29,15 @@ class ArrayAlg {
 	public static <T> T getMiddle(T...a) {
 		return a[a.length / 2];
 	}
+
+	public static <T extends Comparable> T min(T[] a) {
+		if (a == null || a.length == 0) return null;
+		T smallest = a[0];
+		for (int i = 1; i < a.length; i++) {
+			if (smallest.compareTo(a[i]) > 0) {
+				smallest = a[i];
+			}
+		}
+		return smallest;
+	}
 }
