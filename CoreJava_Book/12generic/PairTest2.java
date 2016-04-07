2861f6705d8e8cf99c2d1682127daca21be9de79
muupu <once645@gmail.com>
2016-04-07 11:41:34 +0800

    类型变量的限定

--- CoreJava_Book/12generic/PairTest2.java
+++ CoreJava_Book/12generic/PairTest2.java
@@ -1,28 +1,31 @@
 
-public class PairTest {
+import java.util.*;
+
+public class PairTest2 {
 	public static void main(String[] args) {
 		testString();
 
 	}
 
 	public static void testString() {
-		String[] words = { "Mary", "had", "a", "little", "lamb"};
-		Pair<String> mm = ArrayAlg.minmax(words);
-		System.out.println("min = " + mm.getFirst());
-		System.out.println("max = " + mm.getSecond());
-
-		// String middle = ArrayAlg.<String>getMiddle("John", "Q.", "public");
-		String middle = ArrayAlg.getMiddle(words);
-		System.out.println("middle = " + middle);
+		GregorianCalendar[] birthdays = 
+		{
+			new GregorianCalendar(1906, Calendar.DECEMBER, 9),
+			new GregorianCalendar(1815, Calendar.DECEMBER, 10),
+			new GregorianCalendar(1903, Calendar.DECEMBER, 3),
+			new GregorianCalendar(1910, Calendar.JUNE, 22),
+		};
+		Pair<GregorianCalendar> mm = ArrayAlg.minmax(birthdays);
+		System.out.println("min = " + mm.getFirst().getTime());
+		System.out.println("max = " + mm.getSecond().getTime());
 	}
-
 }
 
 class ArrayAlg {
-	public static Pair<String> minmax(String[] a) {
+	public static <T  extends Comparable> Pair<T> minmax(T[] a) {
 		if (a == null || a.length == 0) return null;
-		String min = a[0];
-		String max = a[0];
+		T min = a[0];
+		T max = a[0];
 		for (int i = 1; i < a.length; i++) {
 			if (min.compareTo(a[i]) > 0) min = a[i];
 			if (max.compareTo(a[i]) < 0) max = a[i];
