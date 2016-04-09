5627329db2bfbf2ed95b41a7e693b99345917d1a
muupu <once645@gmail.com>
2016-04-09 22:33:12 +0800

    test ListIterator

--- CoreJava_Book/13collection/LinkedListTest.java
+++ CoreJava_Book/13collection/LinkedListTest.java
@@ -17,7 +17,21 @@ public class LinkedListTest {
 		System.out.println(staff);
 	}
 
+	public static void testListIterator() {
+		List<String> staff = new LinkedList<>();
+		staff.add("Amy");
+		staff.add("Bob");
+		staff.add("Carl");
+		System.out.println(staff);
+
+		ListIterator<String> iter = staff.listIterator();
+		iter.next();
+		iter.add("Juliet");
+		System.out.println(staff);
+	}
+
 	public static void main(String[] args) {
-		testAddDel();
+		//testAddDel();
+		testListIterator();
 	}
 }
