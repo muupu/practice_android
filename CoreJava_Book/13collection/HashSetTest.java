b9f8006c9c78dc0432be838078e51bc75eea2123
muupu <once645@gmail.com>
2016-04-09 23:22:30 +0800

    HashSetTest

---
+++ CoreJava_Book/13collection/HashSetTest.java
@@ -0,0 +1,41 @@
+
+import java.util.*;
+
+public class HashSetTest {
+
+	public static void testHashSet() {
+		List<String> a = new LinkedList<>();
+		a.add("Amy");
+		a.add("Carl");
+		a.add("Erica");
+		a.add("Bob");
+		a.add("Doug");
+		a.add("Frances");
+		a.add("Gloria");
+		System.out.println(a);
+
+		Set<String> words = new HashSet<>();
+		long totalTime = 0;
+
+		Iterator<String> aIter = a.iterator();
+		while (aIter.hasNext()) {
+			String name = aIter.next();
+			long callTime = System.currentTimeMillis();
+			words.add(name);
+			callTime = System.currentTimeMillis() - callTime;
+			totalTime += callTime;
+		}
+
+		Iterator<String> iter = words.iterator();
+		while (iter.hasNext()) {
+			System.out.println(iter.next());
+		}
+		System.out.println("...");
+		System.out.println(words);
+		System.out.println(words.size() + " distinct words. " + totalTime + "milliseconds.");
+	}
+
+	public static void main(String[] args) {
+		testHashSet();
+	}
+}
