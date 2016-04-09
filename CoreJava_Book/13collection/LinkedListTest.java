c3dbb56a6e5216063a9d5e61903eeabc18fb379b
muupu <once645@gmail.com>
2016-04-09 23:02:53 +0800

    test removeAll

--- CoreJava_Book/13collection/LinkedListTest.java
+++ CoreJava_Book/13collection/LinkedListTest.java
@@ -35,8 +35,49 @@ public class LinkedListTest {
 		System.out.println(staff);
 	}
 
+	public static void testRemoveall() {
+		List<String> a = new LinkedList<>();
+		a.add("Amy");
+		a.add("Carl");
+		a.add("Erica");
+		System.out.println(a);
+
+		List<String> b = new LinkedList<>();
+		b.add("Bob");
+		b.add("Doug");
+		b.add("Frances");
+		b.add("Gloria");
+		System.out.println(b);
+
+		// merge b into a
+		ListIterator<String> aIter = a.listIterator();
+		Iterator<String> bIter = b.iterator();
+		while (bIter.hasNext()) {
+			if (aIter.hasNext()) aIter.next();
+			aIter.add(bIter.next());
+		}
+		System.out.println(a);
+
+		// remove every second word from b
+
+		bIter = b.iterator();
+		while (bIter.hasNext()) {
+			bIter.next(); // skip one element
+			if (bIter.hasNext()) {
+				bIter.next();
+				bIter.remove();
+			}
+		}
+		System.out.println(b);
+
+		// remove all elements in b from a
+		a.removeAll(b);
+		System.out.println(a);
+	}
+
 	public static void main(String[] args) {
 		//testAddDel();
-		testListIterator();
+		//testListIterator();
+		testRemoveall();
 	}
 }
