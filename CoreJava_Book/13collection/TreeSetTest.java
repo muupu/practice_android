6e8d1ff9c7f57c6d2a5b84a9da75a5492bc4d96f
muupu <once645@gmail.com>
2016-04-10 14:11:53 +0800

    SortedSet:使用自定义的比较器

--- CoreJava_Book/13collection/TreeSetTest.java
+++ CoreJava_Book/13collection/TreeSetTest.java
@@ -2,6 +2,7 @@
 import java.util.*;
 
 public class TreeSetTest {
+	
 
 	public static void testTreeSet() {
 		SortedSet<Item> parts = new TreeSet<>();
@@ -11,7 +12,28 @@ public class TreeSetTest {
 		System.out.println(parts);
 	}
 
+	public static void testTreeSet2() {
+		SortedSet<Item> parts = new TreeSet<>();
+		parts.add(new Item("Toaster", 1234));
+		parts.add(new Item("Widget", 4562));
+		parts.add(new Item("Modem", 9912));
+		System.out.println(parts);
+
+		SortedSet<Item> sortByDescription = new TreeSet<>(new 
+			Comparator<Item>()
+			{
+				public int compare(Item a, Item b) {
+					String descrA = a.getDescription();
+					String descrB = b.getDescription();
+					return descrA.compareTo(descrB);
+				}
+			});
+		sortByDescription.addAll(parts);
+		System.out.println(sortByDescription);
+	}
+
 	public static void main(String[] args) {
-		testTreeSet();
+		//testTreeSet();
+		testTreeSet2();
 	}
 }
