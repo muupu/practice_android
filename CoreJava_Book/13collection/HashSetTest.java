6c9b334d0bfc6dbfd3367a265cd8ee9a5f5b5a41
muupu <once645@gmail.com>
2016-04-10 11:32:19 +0800

    将其他集合里的元素全部添加到HashSet中

--- CoreJava_Book/13collection/HashSetTest.java
+++ CoreJava_Book/13collection/HashSetTest.java
@@ -35,7 +35,23 @@ public class HashSetTest {
 		System.out.println(words.size() + " distinct words. " + totalTime + "milliseconds.");
 	}
 
+	public static void testHashSet2() {
+		List<String> a = new LinkedList<>();
+		a.add("Amy");
+		a.add("Carl");
+		a.add("Erica");
+		a.add("Bob");
+		a.add("Doug");
+		a.add("Frances");
+		a.add("Gloria");
+		System.out.println(a);
+
+		Set<String> words = new HashSet<>(a);
+		System.out.println(words);
+	}
+
 	public static void main(String[] args) {
 		testHashSet();
+		testHashSet2();
 	}
 }
