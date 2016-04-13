cbd0cdd30b36d572758fe25c35b2f51d88fa69a9
muupu <once645@gmail.com>
2016-04-13 20:08:03 +0800

    trimString实现

--- UnitTest/app/src/main/java/com/test/unittest/MainActivity.java
+++ UnitTest/app/src/main/java/com/test/unittest/MainActivity.java
@@ -15,8 +15,28 @@ public class MainActivity extends Activity {
 
 //        String str = "ABCABCDEFGHIJKLMNOPQ小米手机";
 //        String str = "ABCABCDEFGH小米手机";
-        String str = "ABCABCDEs小米手机";
-        Log.d("zqd", checkString(str, 8));
+//        String str = "ABCABCDEs小米手机";
+//        String str = "\"RDm4BKQ8PHgAAwHR小米手机\"";
+//        Log.d("zqd", checkSSIDMaxLength(str));
+//        Log.d("zqd", checkSSIDMaxLength("ABCABCDEs小米手机"));
+//        Log.d("zqd", checkSSIDMaxLength("\"AB小米手机CABCDEs\""));
+//        Log.d("zqd", checkSSIDMaxLength("\"ABCABCDEs小米手机\""));
+//        Log.d("zqd", checkSSIDMaxLength(""));
+//        Log.d("zqd", checkSSIDMaxLength("\"\""));
+//        Log.d("zqd", checkSSIDMaxLength("\"小米手机\""));
+
+//        Log.d("zqd", checkString("\"RDm4BKQ8PHgAAwHR小米手机\"", 7));
+//        Log.d("zqd", checkString("ABCABCDEs小米手机", 7));
+//        Log.d("zqd", checkString("\"AB小米手机CABCDEs\"", 7));
+//        Log.d("zqd", checkString("\"ABCABCDEs小米手机\"", 7));
+        String str = checkString("", 7);
+        int l = str.length();
+        Log.d("zqd", str);
+        Log.d("zqd", String.valueOf(l));
+        Log.d("zqd", checkString("", 7));
+//        Log.d("zqd", checkString("\"\"", 7));
+//        Log.d("zqd", checkString("小米手机", 7));
+//        Log.d("zqd", checkString("\"小米手机\"", 7));
     }
 
     //英文占1byte，非英文（可认为是中文）占2byte，根据这个特性来判断字符
@@ -29,10 +49,32 @@ public class MainActivity extends Activity {
         }
     }
 
-    public static String checkString(String str, int maxLength) {
-        StringBuilder res = new StringBuilder();
-        float count = 0.0f;
+    /**
+     * 字符串首尾截除指定的字符
+     * @param str 待截除的字符串
+     * @param ch 制定的字符
+     * @return
+     */
+    public static String trimString(String str, char ch) {
+        int start = 0;
+        int last = str.length() - 1;
+        int end = last;
+        while ((start < end) && (str.charAt(start) == ch)) {
+            start++;
+        }
+        while ((end > start) && (str.charAt(end) == ch)) {
+            end--;
+        }
+        if (start == 0 && end == last) {
+            return str;
+        }
+        return str.substring(start, end + 1);
+    }
+
+    public static String checkString2(String str, int maxLength) {
         if (str != null) {
+            float count = 0.0f;
+            StringBuilder res = new StringBuilder();
             for (int i = 0; i < str.length(); i++) {
                 char c = str.charAt(i);
                 if (!checkChar(c)) {
@@ -46,7 +88,52 @@ public class MainActivity extends Activity {
                 }
                 res.append(c);
             }
+            return res.toString();
+        } else {
+            return null;
         }
-        return res.toString();
+    }
+
+    public static String checkSSIDMaxLength(String ssid) {
+        if (ssid != null) {
+            String str = trimString(ssid, '\"');
+            str = checkString2(str, 7);
+            return "\"" + str +"\"";
+        } else {
+            return null;
+        }
+    }
+
+    public static String checkString(String str, int maxLength) {
+        if (str != null) {
+            float count = 0.0f;
+            StringBuilder res = new StringBuilder();
+            boolean hasDoubleQuotes = false; // 是否有双引号
+            if (str.length() >= 2 && str.charAt(0) == '\"' && str.charAt(str.length()-1) == '\"')
+            {
+                hasDoubleQuotes = true;
+                res.append('\"');
+                str = str.substring(1,str.length()-1);
+            }
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                if (!checkChar(c)) {
+                    count += 1; // 中文
+                } else {
+                    count += 0.5; // 英文
+                }
+                if (count > maxLength) {
+                    res.append("...");
+                    break;
+                }
+                res.append(c);
+            }
+            if (hasDoubleQuotes) {
+                res.append('\"');
+            }
+            return res.toString();
+        } else {
+            return null;
+        }
     }
 }
