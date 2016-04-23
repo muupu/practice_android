74ab0a0df4e3738164d816148590c70b900b2d37
muupu <once645@gmail.com>
2016-04-23 14:52:45 +0800

    懒汉式单例模式

---
+++ EffectiveJava2/3_Singleton/Singleton1.java
@@ -0,0 +1,10 @@
+
+public class Singleton1 {
+	private static final Singleton1 instance = new Singleton1();
+
+	private Singleton1() { }
+
+	public static Singleton1 getInstance() { return instance;}
+
+	
+}
