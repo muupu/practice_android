9ad15180217581602731662a2df9d008e308bd11
muupu <once645@gmail.com>
2016-04-23 17:46:25 +0800

    update Singleton1.java

--- EffectiveJava2/3_Singleton/Singleton1.java
+++ EffectiveJava2/3_Singleton/Singleton1.java
@@ -3,7 +3,7 @@ public class Singleton1 {
 	private static Singleton1 instance = null; 
 
 	private Singleton1 (){}
-
+ 
 	public static synchronized Singleton1 getInstance() {  
 		if (instance == null) {  
 			instance = new Singleton1();  
