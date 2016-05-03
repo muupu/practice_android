88462568b73b09b57c0ddee4c8e41b31d317adfd
muupu <once645@gmail.com>
2016-05-03 16:58:54 +0800

    test

--- FirstCode_Book/UICustomViews/app/src/main/java/com/test/uicustomviews/MainActivity.java
+++ FirstCode_Book/UICustomViews/app/src/main/java/com/test/uicustomviews/MainActivity.java
@@ -7,14 +7,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private TitleLayout mTitleLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        mTitleLayout = (TitleLayout)findViewById(R.id.main_title);
+
+        mTitleLayout.setOnBackClickListener(new TitleLayout.OnBackClickListener(){
+            @Override
+            public void onBackClick() {
+                Toast.makeText(MainActivity.this, "You clicked Back button", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         Button mainButton = (Button)findViewById(R.id.main_button);
         mainButton.setOnClickListener(new View.OnClickListener() {
--- FirstCode_Book/UICustomViews/app/src/main/java/com/test/uicustomviews/SecondActivity.java
+++ FirstCode_Book/UICustomViews/app/src/main/java/com/test/uicustomviews/SecondActivity.java
@@ -3,16 +3,28 @@ package com.test.uicustomviews;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Toast;
 
 /**
  * Created by qiaoda.zqd on 2016/5/3.
  */
 public class SecondActivity extends Activity {
 
+    private TitleLayout mTitleLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_second);
+
+        mTitleLayout = (TitleLayout)findViewById(R.id.second_title);
+
+        mTitleLayout.setOnBackClickListener(new TitleLayout.OnBackClickListener() {
+            @Override
+            public void onBackClick() {
+                Toast.makeText(SecondActivity.this, "You clicked Back button", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
