6d3396efac6a191491a7283acb6b265abe54b504
qiaoda.zqd <qiaoda.zqd@alibaba-inc.com>
2016-10-23 21:31:38 +0800

    add CalculatorTest

---
+++ UnitTest/app/src/main/java/com/test/unittest/Calculator.java
@@ -0,0 +1,24 @@
+package com.test.unittest;
+
+/**
+ * Created by qiaoda.zqd on 2016/10/23.
+ */
+
+public class Calculator {
+
+    public double sum(double a, double b){
+        return a + b;
+    }
+
+    public double substract(double a, double b){
+        return 0;
+    }
+
+    public double divide(double a, double b){
+        return 0;
+    }
+
+    public double multiply(double a, double b){
+        return 0;
+    }
+}
--- UnitTest/app/src/test/java/com/test/unittest/StringUtilsTest.java
+++ UnitTest/app/src/test/java/com/test/unittest/StringUtilsTest.java
@@ -1,12 +1,15 @@
 package com.test.unittest;
 
-import android.test.AndroidTestCase;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
 
 /**
  * Created by qiaoda.zqd on 2016/4/14.
  */
-public class StringUtilsTest extends AndroidTestCase {
-
+public class StringUtilsTest {
+    @Test
     public void testTrimString() throws Exception {
         assertEquals("ABC", StringUtils.trimString("ABC", ' '));
         assertEquals("ABC", StringUtils.trimString("ABC ", ' '));
