d8a952a3b6811c645aa16610692fbad563109d84
muupu <once645@gmail.com>
2016-10-24 23:34:57 +0800

    junit4测试

---
+++ UnitTest/app/src/test/java/com/test/unittest/EmailValidatorTest.java
@@ -0,0 +1,48 @@
+package com.test.unittest;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by qiaoda.zqd on 2016/10/24.
+ */
+@SmallTest
+public class EmailValidatorTest {
+    @Test
+    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("name@email.com"));
+    }
+
+    @Test
+    public void emailValidator_CorrectEmailSubDomain_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("name@email.co.uk"));
+    }
+
+    @Test
+    public void emailValidator_InvalidEmailNoTld_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@email"));
+    }
+
+    @Test
+    public void emailValidator_InvalidEmailDoubleDot_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@email..com"));
+    }
+
+    @Test
+    public void emailValidator_InvalidEmailNoUsername_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("@email.com"));
+    }
+
+    @Test
+    public void emailValidator_EmptyString_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail(""));
+    }
+
+    @Test
+    public void emailValidator_NullEmail_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail(null));
+    }
+}
