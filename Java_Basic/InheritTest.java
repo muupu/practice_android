73b83c11ce273de2fb941f11f0af2d28e050e294
muupu <once645@gmail.com>
2016-11-18 23:45:37 +0800

    add log

--- Java_Basic/InheritTest.java
+++ Java_Basic/InheritTest.java
@@ -9,12 +9,14 @@ public class InheritTest {
 		private int count;
 		
 		public void add(int number){
+			System.out.println("Base.add()");
 			if(count<MAX_NUM){
 				arr[count++] = number;    
 			}
 		}
 		
 		public void addAll(int[] numbers){
+			System.out.println("Base.addAll()");
 			for(int num : numbers){
 				add(num);
 			}
@@ -27,12 +29,14 @@ public class InheritTest {
 
 		@Override
 		public void add(int number) {
+			System.out.println("Child.add()");
 			super.add(number);
 			sum+=number;
 		}
 
 		@Override
 		public void addAll(int[] numbers) {
+			System.out.println("Child.addAll()");
 			super.addAll(numbers);
 			for(int i=0;i<numbers.length;i++){
 				sum+=numbers[i];
